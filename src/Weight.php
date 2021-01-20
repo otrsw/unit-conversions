8a9dade3097e57f9dcf3d2d66492dac389543f3b
otrsw <otrsw@users.noreply.github.com>
2021-01-20 02:24:29 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,21 +4,20 @@ namespace Otrsw\UnitConversions;
 
 class Weight
 {
-
-
     private $kilograms;
 
-    public static function fromKilograms(float $kilograms) : self {
+    public static function fromKilograms(float $kilograms) : self
+    {
         return new static($kilograms);
     }
 
     public function __construct(float $kilograms)
     {
-                
         $this->kilograms = $kilograms;
     }
 
-    public function toLbs() : float {
-       return $this->kilograms * 2.204623;   
+    public function toLbs() : float
+    {
+        return $this->kilograms * 2.204623;
     }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,17 +2,16 @@
 
 namespace Otrsw\UnitConversions\Tests;
 
-use PHPUnit\Framework\TestCase;
 use Otrsw\UnitConversions\Weight;
+use PHPUnit\Framework\TestCase;
 
 class WeightTest extends TestCase
 {
     /** @test */
-    public function it_can_convert_kilogram_to_lbs() {
-        
+    public function it_can_convert_kilogram_to_lbs()
+    {
         $lbs = Weight::fromKilograms(100)->toLbs();
 
         $this->assertEquals(220.4623, $lbs);
-
     }
 }
